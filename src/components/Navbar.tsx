fd621d2ee4143346da68d2115d421f4cdef1b782
aramind <88814622+aramind@users.noreply.github.com>
2025-08-11 00:12:54 +0800

    feat: setting up project and the initial navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { HomeIcon, Sprout } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <nav className="sticky top-0 w-full border-b bg-background/96 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex justify-between items-center h-16">
+          {/* LOGO */}
+          <div className="flex items-center">
+            <Link
+              href="/"
+              className="text-xl font-bold text-primary font-mono tracking-wider"
+            >
+              🪴 Plantventory
+            </Link>
+            {/* Navbar components */}
+          </div>
+          <div className="hidden md:flex items-center space-x-4">
+            <Button variant="ghost" className="flex items-center gap-2" asChild>
+              <Link href="/plants">
+                <Sprout className="w-4 h-4" />
+                <span className="hidden md:inline">Pant</span>
+              </Link>
+            </Button>
+            <Button variant="ghost" className="flex items-center gap-2" asChild>
+              <Link href="/">
+                <HomeIcon className="w-4 h-4" />
+                <span className="hidden md:inline">Home</span>
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
