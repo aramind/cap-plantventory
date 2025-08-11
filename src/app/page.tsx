d8241dc3db58e2fab032a1e224858cca9e38712b
aramind <88814622+aramind@users.noreply.github.com>
2025-08-11 13:16:46 +0800

    feat: adding hero landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Hero } from "@/components/Hero";
+
 export default function Home() {
   return (
-    <>
-      <h1>Hello!</h1>
-    </>
+    <div className="mx-auto">
+      <Hero />
+    </div>
   );
 }
